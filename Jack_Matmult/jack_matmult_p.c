b82c8bab11570f51d7e0638020caed866b14d8cd
Jack Belmont <jackbelmont8@gmail.com>
2018-11-04 15:15:25 -0500

    Update to jack_matmult_p
    updated so everything is dynamically alocated to account for when we get our information from the command line

--- Jack_Matmult/jack_matmult_p.c
+++ Jack_Matmult/jack_matmult_p.c
@@ -77,12 +77,12 @@ int main()
 {
 	int errnum;
 
+// FIND THIS PART DYNAMICALLY FROM COMMAND LINE ///////
 	/* (n x m) * (m x p) = (n x p) */
 	const int n = 2;
 	const int m = 3;
 	const int p = 2;
 
-// DO THIS PART DYNAMICALLY FROM COMMAND LINE ///////
 	/*mat 1 with n=2 rows and m=3 columns*/
 	int mat1[n][m] = 	{{1 , 2 , 3},
 						 {4 , 5 , 6}};
@@ -91,10 +91,13 @@ int main()
 						 {9 , 10},
 						 {11 , 12}};
 /////////////////////////////////////////////////////
+	int i;
 
 	//initialize a 2d array for the result of n x p size
-	int* resultMat[n];
-	int i;
+	//dynamically allocated because we dont know the size
+	//before compile time
+	int** resultMat = (int**)malloc(n * sizeof(int*));
+	memset(resultMat, 0, (n * sizeof(int*)));
 	for(i = 0; i<n; i++){
 		//p collumns in every row
 		resultMat[i] = (int*)malloc(p * sizeof(int));
@@ -103,11 +106,36 @@ int main()
 
 //parallel Processing time: /////////////////////////////////////////////////////////////////////////
 
-	pid_t pid[n][p]; // we will be running n*p processes to fill in result matrix
-	int pipeEnds[n][p][2]; // we will be needing n*p pipes to communicate with n*p processes
+	// we will be running n*p processes to fill in result matrix
+	// so we need n*p pid's 
+	// Dynamically Allocated: pid[n][p]
+	pid_t** pid = (pid_t**) malloc(n * sizeof(pid_t*));
+	memset(pid, 0, (n * sizeof(pid_t*)));
+	for(i = 0; i < n; i++){
+		pid[i] = (pid_t*)malloc(p * sizeof(pid_t));
+		memset(pid[i], 0, p* sizeof(pid_t));
+	}
+
+	// we will be needing n*p pipes to communicate with n*p processes
+	// dynamically allocated: pipeEnds[n][p][2]
 	int j;
+	int*** pipeEnds = (int***) malloc(n * sizeof(int**));
+	memset(pipeEnds, 0, (n * sizeof(int**)));
+	for(i = 0; i < n; i++){
+		pipeEnds[i] = (int**)malloc(p * sizeof(int*));
+		memset(pipeEnds[i], 0, (p * sizeof(int*)));
+		for(j = 0; j < 2; j++){
+			pipeEnds[i][j] = (int*)malloc(2 * sizeof(int));
+			memset(pipeEnds[i][j], 0, (2 * sizeof(int)));
+		}
+	}
+
+	//vector to hold column of mat2
+	//Dynamically Alocated: int mat2col[m]; 
+	int* mat2col = (int*)malloc(m * sizeof(int));
+	memset(mat2col, 0, m * sizeof(int));
+
 	int k;
-	int mat2j[m]; //vector to hold column of mat2
 	int childResult;
 	char parentResult[2048];
 	for(i = 0; i < n; i++){ //create one process for each 
@@ -129,10 +157,10 @@ int main()
 
 				//create the column vector of mat2
 				for (k = 0; k < m; k++){
-					mat2j[k] = mat2[k][j];
+					mat2col[k] = mat2[k][j];
 				}
 
-				childResult = vectorMult(mat1[i], mat2j, m);
+				childResult = vectorMult(mat1[i], mat2col, m);
 				write(pipeEnds[i][j][1], intToString(childResult), strlen(intToString(childResult)));
 
 				//close the pipe;
@@ -154,9 +182,10 @@ int main()
 		}
 	}
 
+	
+	//check to make sure all of our child processes have finished before we go on:
 	pid_t waitID;
 	int status;
-	//check to make sure all of our child processes have finished before we go on:
 	for(i = 0; i<n; i++){
 		for(j = 0; j<p; j++){
 			waitID = waitpid(pid[i][j], &status, WNOHANG);
@@ -176,14 +205,29 @@ int main()
 
 //Done Parallel Processing///////////////////////////////////////////////////////////////////////////////
 
-
-
+//print the resultant matrix to stdout: 
 	for(i = 0; i < n; i++){ 
 		for(j = 0; j < p; j++){
 			printf("%d ", resultMat[i][j]);
 		}
 		printf("\n");
 	}
+
+//free dynamically allocated memory: /////////////////////////////////////////////////////////////////////
+	for(i = 0; i < n; i++){
+		free(resultMat[i]);
+		free(pid[i]);
+		for(j = 0; j < 2; j++){
+			free(pipeEnds[i][j]);
+		}
+		free(pipeEnds[i]);
+	}
+	free(resultMat);
+	free(pid);
+	free(pipeEnds);
+	free(mat2col);
+
+//End the program execution ///////////////////////////////////////////////////////////////////////////////
 
 
 	return 0;
